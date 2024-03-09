f8060f89fc0385bbbb67f9c088eb0cefe35f6eb9
adolfojb2 <adolfojb2@gmail.com>
2024-03-09 11:48:06 -0500

    Se agrega la configuracion para conectar mysql con Django y se realiza la primera migración.

--- Backend/todocarro/settings.py
+++ Backend/todocarro/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'todocarro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todocarro',
+        'USER': 'root',
+        'PASSWORD': 'Ing_096betin',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
